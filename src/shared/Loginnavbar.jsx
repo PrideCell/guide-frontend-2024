8e6c74923836664cb68c98f2ff43c277a5649e39
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-07-06 10:28:29 +0530

    commit

--- src/shared/Loginnavbar.jsx
+++ src/shared/Loginnavbar.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import sistlogologin from "../assets/sistlogologin.png"
+
+
+
 const Loginnavbar = () => {
   return (
     <>
-<nav class="flex items-center justify-between bg-red-900 p-2">
+  
+<nav class="flex items-center justify-between bg-[#9e1c3f] p-2">
   <div class="flex items-center">
     <img src={sistlogologin} alt="Logo" class="h-12 w-40 px-3" />
   </div>
   <div class="flex items-center mx-3">
-    <p class=" font-medium">STAFF</p>
-    <p class="ml-4  font-medium">STUDENT</p>
+  <a href='./About'>ABOUT</a>
+
   </div>
 </nav>
 
