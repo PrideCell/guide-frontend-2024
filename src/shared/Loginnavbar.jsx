62c8762270b643bb0830bc040cb74d426fb74deb
Geddada Pradeep <geddadavenkatapradeep@gmail.com>
2023-07-06 10:53:50 +0530

    commit

--- src/shared/Loginnavbar.jsx
+++ src/shared/Loginnavbar.jsx
@@ -9,10 +9,10 @@ const Loginnavbar = () => {
   
 <nav class="flex items-center justify-between bg-[#9e1c3f] p-2">
   <div class="flex items-center">
-    <img src={sistlogologin} alt="Logo" class="h-12 w-40 px-3" />
+    <a href='/'><img src={sistlogologin} alt="Logo" class="h-12 w-40 px-3" /></a>
   </div>
   <div class="flex items-center mx-3">
-  <a href='./About'>ABOUT</a>
+  <a href='/about'>ABOUT</a>
 
   </div>
 </nav>
